5c973f8e14e2941a8eeec5dcc3ad9c9d5ec7706f
Shadrin Igor <shadrin.igor@gmail.com>
2012-09-27 01:26:04 +0500

    =

---
+++ protected/components/RelationHelper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Колоюок
+ * Date: 27.09.12
+ * Time: 1:20
+ * To change this template use File | Settings | File Templates.
+ */
+class RelationHelper
+{
+    static function getRelation( $relationArray )
+    {
+
+    }
+}
