e127386efe6d051c0dafa5093d751c6426d501df
Shadrin Igor <shadrin.igor@gmail.com>
2012-09-28 14:27:17 +0500

    Веду работу над обработкой картинок

---
+++ protected/components/ImageHelper.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * User: Игорь
+ * Date: 28.09.12
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ImageHelper
+{
+    /*
+     * Проверяем существование картинки если нет то выдает тефолтную картинку
+     * @param string $imageUrl урл картинки
+     * @param integer $size размер необходимой картинки
+     * @pram string $className название класса, чтобы сохранять изменные варинаты в базе ( в талице обязательно должны быть свойства image_2 и image_3 )
+     * @return bool $cout
+     */
+    static function getImageResize( $imageUrl, $size = null, $itemObject = null )
+    {
+        if( !empty( $size ) )$size .= "_";
+
+        if( !empty( $itemObject ) && is_object( $itemObject ) )
+        {
+            $image = "image".$size;
+
+            // Если размер не указан то проверяем производилась ли проверка этого поля ранее
+            // ( если поле image_2 заполненно то значит проверка уже проводилась )
+            if( empty( $size ) && !empty( $itemObject->image_2 ) )return $imageUrl;
+
+            // Если необходимый размер есть среди обработанных то просто выдаем;
+            if( !empty( $itemObject->$image ) )return $itemObject->$image;
+        }
+
+        $fileName = basename( $imageUrl );
+        $dirPath = dirname( $imageUrl );
+        if( !empty( $size ) )$outFile = $dirPath ."/".$size."_".$fileName;
+        else $outFile = $dirPath ."/".$fileName;
+
+        if( !empty( $imageUrl ) && file_exists( $imageUrl ) )
+        {
+            if( !empty( $className ) && $className=="CatalogNews" )
+            {
+
+            }
+            return $imageUrl;
+        }
+
+        // Картинки по умочанию
+        return Yii::app()->getTheme()->getBaseUrl()."/images/no-image.png";
+    }
+}
--- protected/components/views/news.php
+++ protected/components/views/news.php
@@ -1,6 +1,6 @@
 <div class="cItem listItems">
     <a title="<?= $values->name ?> - Узбекистан Шоубиз" href="<?= $url ?>"><?= $values->name ?></a>
-    <div class="TN_img"><div class="count_shows">Просмотров <?= $values->col ?></div><img alt="<?= $values->name ?> - Шоубиз Узбекистан" title="<?= $values->name ?> - Шоубиз Узбекистан" src="f/catalog_news/2012/9/3_17923koncert.jpg"></div>
+    <div class="TN_img"><div class="count_shows">Просмотров <?= $values->col ?></div><img alt="<?= $values->name ?> - Шоубиз Узбекистан" title="<?= $values->name ?> - Шоубиз Узбекистан" src="<?= ImageHelper::getCheckImage( $values->image ) ?> "></div>
 
     <font><?= SiteHelper::getDateOnFormat( $values->date, "d.m.Y", "news ID: ".$values->id ) ?></font><br>
     <a class="newsCid" title="<?= $values->country->name ?>, <?= $values->cid_id->name ?>" href="http://www.world-news.uz/category/9/1/"><?= $values->country->name ?></a>,
