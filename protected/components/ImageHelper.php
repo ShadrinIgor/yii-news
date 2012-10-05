861d25d46c8d0452476240f5c165d2a123f5cbfa
Shadrin Igor <shadrin.igor@gmail.com>
2012-10-06 02:01:56 +0500

    gg

--- protected/components/ImageHelper.php
+++ protected/components/ImageHelper.php
@@ -48,4 +48,208 @@ class ImageHelper
         // Картинки по умочанию
         return Yii::app()->getTheme()->getBaseUrl()."/images/no-image.png";
     }
+
+    public function sheck_file_name($text)
+    {
+        $rus=array("а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ш","щ","ь","ъ","э","ю","я"," ",".","-","(",")","j","w");
+        $eng=array("a","b","v","g","d","e","e","sh","z","i","i","k","l","m","n","o","p","r","s","t","u","f","h","c","sh","sch","","","e","yu","ya","_",".","_","(",")","j","w");
+        $text=strtolower($text);
+
+        $str="";
+        for($n=0;$n<strlen($text);$n++)
+        {
+            for($i=0;$i<sizeof($rus);$i++)
+            {
+                if($text[$n]==$rus[$i])
+                {
+                    $str.=$eng[$i];
+                    break;
+                }
+
+                if($text[$n]==$eng[$i])
+                {
+                    $str.=$eng[$i];
+                    break;
+                }
+
+                if(intval($text[$n]))
+                {
+                    $str.=$text[$n];
+                    break;
+                }
+            }
+        }
+
+        $ar=explode(".",$str);
+        $type=$ar[sizeof($ar)-1];
+
+        $rn=rand(1,999);
+        $str=$rn.$ar[0].".".$type;
+
+        return $str;
+    }
+
+    public function optimization($file,$path,$id_type,$itypre="", $dopUrl='')
+    {
+        $dir=dirname($path);
+
+        $fileName = basename( $file );
+        $dirPath = $dopUrl.dirname( $file )."/";
+
+        $res=mysql_query("SELECT * FROM img_type WHERE id='".$id_type."'");
+        $line=mysql_fetch_array($res);
+
+        list($width0,$height0)=getimagesize($path);
+
+        $wprocent = ceil( ($height0*100)/ $width0 );
+        $hprocent = ceil( ($width0*100)/ $height0 );
+
+        for($i=3;$i>0;$i--)
+        {
+            $width=0;
+            $height=0;
+            if($i==1)
+            {
+                $new_file_name = $dirPath . $fileName;
+
+                $width=$line['width'];
+                $height=$line['height'];
+                if(!$width&&!$height)list($width,$height)=getimagesize($path);
+
+                if( !$width && $height )$width= ceil( ($height*$hprocent)/100 );
+                if( $width && !$height )$height= ceil( ($width*$wprocent)/100 );
+            }
+            else
+            {
+                $new_file_name = $dirPath.$i."_".$fileName;
+                $height=$line['height'.$i];
+                $width=$line['width'.$i];
+
+                if( $width0 == $height )
+                {
+                    if( !$width && $height )$width=$height;
+                    if( $width && !$height )$height=$width;
+                }
+
+                if( !$width && $height )$width= ceil( ($height*$hprocent)/100 );
+                if( $width && !$height )$height= ceil( ($width*$wprocent)/100 );
+            }
+
+//         echo $dopUrl.$file." - |".$itypre."|<br/>";
+
+            if( $width&&$height )
+            {
+                switch($itypre)
+                {
+                    case "jpg" :$image_o=imagecreatefromjpeg( $dopUrl.$file);break;
+                    case "image/jpg" :$image_o=imagecreatefromjpeg($dopUrl.$file);break;
+
+                    case "jpeg":$image_o=imagecreatefromjpeg($dopUrl.$file);break;
+                    case "image/jpeg":$image_o=imagecreatefromjpeg($dopUrl.$file);break;
+                    case "image/pjpeg":$image_o=imagecreatefromjpeg($dopUrl.$file);break;
+
+                    case "gif" :$image_o=imagecreatefromgif($dopUrl.$file);break;
+                    case "image/gif" :$image_o=imagecreatefromgif($dopUrl.$file);break;
+
+                    case "png" :$image_o=imagecreatefrompng($dopUrl.$file);break;
+                    case "image/png" :$image_o=imagecreatefrompng($dopUrl.$file);break;
+
+                    default : $image_o=imagecreatefromjpeg( $dopUrl.$file);break;
+                }
+
+                list($width_o,$height_o)=getimagesize($path);
+                $new_file=imagecreatetruecolor($width,$height);
+                $res=imagecopyresampled($new_file,$image_o,0,0,0,0,$width,$height,$width_o,$height_o);
+
+                if($res===True)
+                {
+                    if( $i<3 )$compressing = 70;
+                    else $compressing = 50;
+
+                    switch( $itypre )
+                    {
+                        case "jpeg":ImageJPEG($new_file,$new_file_name, $compressing);break;
+                        case "image/jpeg":ImageJPEG($new_file,$new_file_name, $compressing);break;
+                        case "image/pjpeg":ImageJPEG($new_file,$new_file_name, $compressing);break;
+
+                        case "jpg":ImageJPEG($new_file,$new_file_name, $compressing);break;
+                        case "image/jpg":ImageJPEG($new_file,$new_file_name, $compressing);break;
+
+                        case "gif":ImageGIF($new_file,$new_file_name);break;
+                        case "image/gif":ImageGIF($new_file,$new_file_name);break;
+
+                        case "png":ImagePNG($new_file,$new_file_name);break;
+                        case "image/png":ImagePNG($new_file,$new_file_name);break;
+                    }
+                }
+                else
+                    $cout.="<p class=\"err\">Произошла ошибка обработки файла (".$new_file_name.")</p>";
+
+                #Наложение логотипа на картинки
+                if( trim(getLocalVaribal("addLogoOnImage")) )$this->addLogoOnImage( $new_file_name, $itypre, $dopUrl.getLocalVaribal("logo") );
+            }
+        }
+
+        return $cout;
+    }
+
+    function addLogoOnImage( $tempImage, $imgType, $logo )
+    {
+        switch( $imgType )
+        {
+            case "jpeg":$srcImage = ImageCreateFromJPEG($tempImage);break;
+            case "image/jpeg":$srcImage = ImageCreateFromJPEG($tempImage);break;
+            case "image/pjpeg":$srcImage = ImageCreateFromJPEG($tempImage);break;
+
+            case "jpg":$srcImage = ImageCreateFromJPEG($tempImage);break;
+            case "image/jpg":$srcImage = ImageCreateFromJPEG($tempImage);break;
+
+            case "gif":$srcImage = ImageCreateFromGIF($tempImage);break;
+            case "image/gif":$srcImage = ImageCreateFromGIF($tempImage);break;
+
+            case "png":$srcImage = ImageCreateFromPNG($tempImage);break;
+            case "image/png":$srcImage = ImageCreateFromPNG($tempImage);break;
+            default:return -1;break;
+        }
+
+        $logoImage = imagecreatefrompng( $logo );
+
+        $srcWidth  = ImageSX($srcImage);
+        $srcHeight = ImageSY($srcImage);
+
+        $logoWidth  = ImageSX($logoImage);
+        $logoHeight = ImageSY($logoImage);
+
+        $newLogoWeight = $logoWidth;
+        $newlogoHeightt = $logoHeight;
+
+        if( $logoWidth>$srcWidth )
+        {
+            $newLogoWeight = $srcWidth;
+            $pr = (100*$srcWidth)/$logoWidth;
+            $newlogoHeightt = ($logoHeight*$pr)/100;
+        }
+
+        $res = imagecopyresized($srcImage, $logoImage, 0, $srcHeight - $newlogoHeightt, 0, 0, $newLogoWeight, $newlogoHeightt, $logoWidth, $logoHeight);
+
+        unlink($tempImage);
+
+        switch( $imgType )
+        {
+            case "jpeg":ImageJPEG($srcImage, $tempImage, 100);break;
+            case "image/jpeg":ImageJPEG($srcImage, $tempImage, 100);break;
+            case "image/pjpeg":ImageJPEG($srcImage, $tempImage, 100);break;
+
+            case "jpg":ImageJPEG($srcImage, $tempImage, 100);break;
+            case "image/jpg":ImageJPEG($srcImage, $tempImage, 100);break;
+
+            case "gif":ImageGIF($srcImage, $tempImage);break;
+            case "image/gif":ImageGIF($srcImage, $tempImage);break;
+
+            case "png":ImagePNG($srcImage, $tempImage);break;
+            case "image/png":ImagePNG($srcImage, $tempImage);break;
+        }
+
+        ImageDestroy($srcImage);
+    }
 }
