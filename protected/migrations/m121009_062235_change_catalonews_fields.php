0c9b70da2de4d92a1bdb4036daf71bca756b562a
Shadrin Igor <shadrin.igor@gmail.com>
2012-10-09 11:31:11 +0500

    Создал миграцию - Изминения поле таблицы catalog_news

---
+++ protected/migrations/m121009_062235_change_catalonews_fields.php
@@ -0,0 +1,27 @@
+<?php
+
+class m121009_062235_change_catalonews_fields extends CDbMigration
+{
+	public function up()
+	{
+        $sql = file_get_contents(dirname(__FILE__).DIRECTORY_SEPARATOR.__CLASS__.'_up.sql');
+        $this->getDbConnection()->createCommand($sql)->execute();
+	}
+
+	public function down()
+	{
+		echo "m121009_062235_change_catalonews_fields does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
