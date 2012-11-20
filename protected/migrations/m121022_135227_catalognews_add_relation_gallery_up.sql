71b89a2d007902541be579cf7fcc3c83a575e1da
Shadrin Igor <shadrin.igor@gmail.com>
2012-11-20 11:52:51 +0500

    изменил играции

--- protected/migrations/m121022_135227_catalognews_add_relation_gallery_up.sql
+++ protected/migrations/m121022_135227_catalognews_add_relation_gallery_up.sql
@@ -1,3 +1,1 @@
-ALTER TABLE `img_gallery` ENGINE = InnoDB;
-INSERT INTO `yii-news`.`img_gallery` (`id`, `type`, `img`, `img2`, `img3`, `table_`, `lang_group`, `id_lang`, `cid`, `link`, `title`, `flash`) VALUES ('2147483647', '0', '', '', '', '', '0', '0', '0', '', '', '0');
-UPDATE `yii-news`.`img_gallery` SET `id` = '0' WHERE `img_gallery`.`id` =2147483647;
+;
--- protected/migrations/m121023_102448_cat_gallery_up.sql
+++ protected/migrations/m121023_102448_cat_gallery_up.sql
@@ -1,4 +1,4 @@
-RENAME TABLE `yii-news`.`img_gallery` TO `yii-news`.`cat_gallery` ;
+RENAME TABLE `img_gallery` TO `cat_gallery` ;
 ALTER TABLE `cat_gallery` ENGINE = InnoDB;
 ALTER TABLE `cat_gallery` DROP `lang_group` , DROP `id_lang` ;
 ALTER TABLE `cat_gallery` CHANGE `flash` `flash` INT( 1 ) NULL DEFAULT '0';
@@ -13,3 +13,6 @@ ALTER TABLE `cat_gallery` ADD `del` INT NOT NULL DEFAULT '0';
 ALTER TABLE `cat_gallery` CHANGE `img` `image` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
 CHANGE `img2` `image_2` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
 CHANGE `img3` `image_3` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+
+INSERT INTO `cat_gallery` (`id`, `type`, `img`, `img2`, `img3`, `table_`, `lang_group`, `id_lang`, `cid`, `link`, `title`, `flash`) VALUES ('2147483647', '0', '', '', '', '', '0', '0', '0', '', '', '0');
+UPDATE `cat_gallery` SET `id` = '0' WHERE `img_gallery`.`id` =2147483647;
