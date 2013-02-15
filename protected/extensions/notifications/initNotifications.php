5216d48d7278496b7d4642efbe967c3e32796bba
ShadrinIgor <shadrin.igor@gmai.com>
2013-02-16 02:27:30 +0500

    добавил нотификацию

--- protected/extensions/notifications/initNotifications.php
+++ protected/extensions/notifications/initNotifications.php
@@ -13,6 +13,32 @@ class initNotifications extends CApplicationComponent
         Yii::import("ext.notifications.models.*");
     }
 
+    public function send( $key, $type, array $arrayParams = array() )
+    {
+        $notification = Notifications::fetchAll(
+                DBQueryParamsClass::CreateParams()
+                    ->setConditions( "key=:key" )
+                    ->setParams( array( ":key"=>$key ) )
+            );
 
+        if( !empty( $notification ) && sizeof( $notification )>0 )
+        {
+            $notificationItems = NotificationsItems::fetchAll(
+                DBQueryParamsClass::CreateParams()
+                    ->setConditions( "notification_id=:notification_id" )
+                    ->setParams( array( ":notification_id"=>$notification->id ) )
+            );
+
+            if( !empty( $notificationItems ) && sizeof( $notificationItems )>0 )
+            {
+                for( $i=0;$i<sizeof( $notificationItems );$i++ )
+                {
+
+                }
+            }
+                else return false;
+        }
+            else return false;
+    }
 }
 ?>
